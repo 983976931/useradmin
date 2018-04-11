63a96400d2a9ef2815fec7b0a2ff7e55f7ef512e
LeeYangyang <983976931@qq.com>
2018-04-11 16:17:25 +0800

    Initial commit

---
+++ src/main/java/com/lyy/useradmin/UseradminApplication.java
@@ -0,0 +1,12 @@
+package com.lyy.useradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseradminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseradminApplication.class, args);
+	}
+}
